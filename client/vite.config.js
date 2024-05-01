fd1eb373735243b5cf560fbd53760cb9e7abc967
Nattawat Pitikomon <nattawat.arch@gmail.com>
2024-05-01 18:49:13 +0900

    setup client

---
+++ client/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV), //Defines the apps environment
+    },
+  };
+});
