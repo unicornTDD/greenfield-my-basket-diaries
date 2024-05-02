15f71cc716e9f72b778267c778a45f8706ef1a62
Nattawat Pitikomon <nattawat.arch@gmail.com>
2024-05-02 12:13:21 +0900

    install @MUI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React, { useState } from "react";
 import "./App.css";
 
-const BASE_URL = import.meta.env.VITE_BASE_URL
+// @MUI
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import Button from "@mui/material/Button";
+import { CardHeader } from "@mui/material";
+
+// LOCAL COMPONENTS
+import Header from "./components/Header";
+
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      {/* HEADER */}
+      <div className="header">
+        <h1>GREEN FIELD MY BASKET DIARY</h1>
+        <p>Username</p>
       </div>
-      <h1>Green Field Project {BASE_URL}</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      {/* CONTENT */}
     </>
   );
 }
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import { Dropdown } from "@mui/base/Dropdown";
+import { MenuButton } from "@mui/base/MenuButton";
+import { Menu } from "@mui/base/Menu";
+import { MenuItem } from "@mui/base/MenuItem";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { CardHeader } from "@mui/material";
+
+const Header = () => {
+  return {
+    /* HEADER */
+  };
+};
+
+export default Header;
