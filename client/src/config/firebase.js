8b8ded2d35ba55ed5f2406c9eadaa4daa8184ef0
Deana Chou <130136996+deanachou@users.noreply.github.com>
2024-05-03 17:54:26 +0900

    firebase config file created

---
+++ client/src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB544ckh-_U7w62PljZlQpXmotDp2UfVTw",
+  authDomain: "my-basket-diaries.firebaseapp.com",
+  projectId: "my-basket-diaries",
+  storageBucket: "my-basket-diaries.appspot.com",
+  messagingSenderId: "847696439485",
+  appId: "1:847696439485:web:2f2546aef1ee02e70defd4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
