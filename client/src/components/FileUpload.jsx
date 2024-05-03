b5e9a9bd409d1fb13a74a4a4cb3820076dd8d627
Deana Chou <130136996+deanachou@users.noreply.github.com>
2024-05-03 21:56:56 +0900

    uploadFile function also gets img path url

--- client/src/components/FileUpload.jsx
+++ client/src/components/FileUpload.jsx
@@ -1,12 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import "./FileUpload.css";
 import { storage } from "../config/firebase";
-import { ref, uploadBytes } from "firebase/storage";
+import {
+  getStorage,
+  ref,
+  uploadBytes,
+  listAll,
+  getDownloadURL,
+} from "firebase/storage";
+//built in firebase functions.
+//ref: create reference to the folder path on the cloud storage
+//uploadBytes: uploads the photo, takes care of file type etc. magic.
+//listAll: list all items (objects) in the path folder specified
+//getDownloadURL: get the actual url that can be added to img src to display the image
 
 export default function FileUpload() {
   //file upload state
   const [fileUpload, setFileUpload] = useState(null);
+  const [imageList, setImageList] = useState([]);
+
+  //use effect
+  useEffect(() => {
+    listAllImages();
+  }, []);
 
   //handler function
+  const storage = getStorage();
+  const imageListRef = ref(storage, "diaryEntries/");
+
+  //upload a file to the bucket
   const uploadFile = async () => {
     if (!fileUpload) return;
     const diaryEntriesFolderRef = ref(
@@ -14,22 +36,37 @@ export default function FileUpload() {
       `diaryEntries/${fileUpload.name}`
     );
     try {
-      await uploadBytes(diaryEntriesFolderRef, fileUpload);
+      const snapshot = await uploadBytes(diaryEntriesFolderRef, fileUpload);
+      const downloadURL = await getDownloadURL(snapshot.ref);
+      console.log("Download URL", downloadURL);
     } catch (err) {
-      console.error(err);
+      console.error("image upload error!", err);
     }
+  };
+
+  const listAllImages = async () => {
+    const imageListObj = await listAll(imageListRef);
+
+    const requests = imageListObj.items.map((item) => getDownloadURL(item));
+    const urls = await Promise.all(requests);
+    console.log("listAll images from ref", imageListObj);
+    console.log("requests", requests);
+    console.log("urls", urls);
   };
 
   // RETURN
   return (
     <>
-      <p>file upload componenttt</p>
+      <p>file upload component</p>
       <div>
         <input
           type="file"
           onChange={(e) => setFileUpload(e.target.files[0])}
         ></input>
         <button onClick={uploadFile}>Upload File</button>
+        {imageList.map((url) => {
+          return <img src={url} />;
+        })}
       </div>
     </>
   );
