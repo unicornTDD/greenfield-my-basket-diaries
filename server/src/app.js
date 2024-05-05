3826bb6f3aa289ce4cdce5acf87b412d305a6ccb
Dominik <142134892+dominiksakic@users.noreply.github.com>
2024-05-05 13:43:58 +0900

    PR | Login hotfix#2 (#40)
    * chore: handle user login for non existent user/no crash form server
    
    * chore: new cookie session
    
    * chore: handle user login for non existent user/no crash form server
    
    * chore: new cookie session
    
    * chore: configure hhtps
    
    ---------
    
    Signed-off-by: Dominik <142134892+dominiksakic@users.noreply.github.com>
    Co-authored-by: dominik <dominik@shintakukanadenoMacBook-Air.local>

--- server/src/app.js
+++ server/src/app.js
@@ -19,6 +19,9 @@ app.use(
     name: "session",
     keys: [process.env.SECRET], //ad to teh server env file
     maxAge: 24 * 60 * 60 * 1000,
+    sameSite: "None", // Set SameSite=None
+    secure: true, // Set Secure
+
   })
 );
 
