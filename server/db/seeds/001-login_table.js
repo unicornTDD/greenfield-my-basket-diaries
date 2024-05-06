06c7980733bcaa72543a1152815b0d2f74a23547
dominik <dominik@shintakukanadenoMacBook-Air.local>
2024-05-06 19:49:42 +0900

    re-seed

--- server/db/seeds/001-login_table.js
+++ server/db/seeds/001-login_table.js
@@ -6,23 +6,11 @@ exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex("diary").del();
   await knex("login").del();
-  await knex.raw('ALTER SEQUENCE login_id_seq RESTART WITH 1');
+  await knex.raw("ALTER SEQUENCE login_id_seq RESTART WITH 1");
   await knex("login").insert([
     {
       display_name: "Ter",
       email: "unicornsAreAwesomeTer@email.com",
-      hashed_password: "5555555555",
-      salt: "666",
-    },
-    {
-      display_name: "Dominik",
-      email: "unicornsAreAwesomeDom@email.com",
-      hashed_password: "5555555555",
-      salt: "666",
-    },
-    {
-      display_name: "Deana",
-      email: "unicornsAreAwesomeDna@email.com",
       hashed_password: "5555555555",
       salt: "666",
     },
